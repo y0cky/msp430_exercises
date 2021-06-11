d9a76e86bead74a6c0881c0326ce89dc896487ce
Y0cky <1822612@stud.hs-mannheim.de>
2021-06-11 17:14:55 +0200

    Update UART.asm

--- UART.asm
+++ UART.asm
@@ -12,23 +12,13 @@ StopWDT         mov.w   #WDTPW+WDTHOLD,&WDTCTL   ; Watchdog Timer anhalten
 
 ; +++ Konfiguration der IO-Ports und der Port-Interrupts +++
 
-Port_1
-                mov.b #0x00, &P1IFG   ; Alle P1-IFG's löschen, falls zufällig gesetzt         
-                mov.b #BIT0, &P1DIR   ; nur P1.0 für rote LED als Ausgang konfigurieren (1=Out, 0=In), der Rest des Ports sind Eingänge (der Taster Button_S2 hängt an P1.1)
-                mov.b #BIT1, &P1OUT   ; LED an Port 1.0 ist aus (P1.0=0), PullUp an Port 1.1 für den Button_S2 (P1.1=1)  
-                mov.b #0x00, &P1SEL   ; kompletter Port P1 als normaler IO-Port verfügbar, nichts wird an andere Peripherie abgegeben
-                mov.b #BIT1, &P1REN   ; aktiviere PullUp an P1.1 für Button_S2
-                mov.b #0xff, &P1IES   ; alle Port 1 Interrupts werden auf negative Flanke getriggert (das ist so, weil die Taster auf dem LaunchPad nach Masse gehen)
-                mov.b #Button_S2, &P1IE    ; Nur Taster "Button_S2" für Interrupt freigeben, alle anderen Interruptflags von Port 1 unterdrücken
 
-Port_4          bis.b   #BIT7,&P4DIR            ; Port P4.7 als Ausgang konfigurieren für grüne LED 
-                bis.b   #BIT4,&P4DIR            ; Port P4.4 als Ausgang konfigurieren für UART TxD (zum Senden serieller Daten über diesen Pin)  
-                bis.b   #BIT7,&P4OUT            ; LED an Port 4.7 einschalten zu Programmbeginn (1 = "an")
+Port_4          bis.b   #BIT4,&P4DIR            ; Port P4.4 als Ausgang konfigurieren für UART TxD (zum Senden serieller Daten über diesen Pin)  
                 bis.b   #BIT4,&P4OUT            ; 1 = High = UART Idle State
                 bis.b   #BIT4,&P4SEL            ; P4.4 nicht als normalen Port verwenden sondern an USCI-Modul (=UART) abgeben zum Senden (TxD)   
                 bis.b   #BIT5,&P4SEL            ; P4.5 nicht als normalen Port verwenden sondern an USCI-Modul (=UART) abgeben zum Empfangen von Daten (RxD)        
                 
-                ;mehr ist bei Port 4 nicht nötig da sonst der Port nicht benutzt wird in diesem Programm
+
 UART_A1_config  
                 bis.b   #UCSWRST,&UCA1CTL1      ; Die UART in den Reset-Mode bringen um sie nachfolgend Konfigurieren zu können, siehe UserGuide Seite 894
                 
@@ -52,6 +42,7 @@ Register_Init   ;
                 mov.w   #0x00, R8               ; ASCII 3
                 mov.w   #0x00, R10              ; Sum
                 
+                
 main                                            ; Mainloop
                 nop
                 BIS.W #GIE, SR                  ; global Interruptsystem aktivieren
@@ -59,28 +50,6 @@ main                                            ; Mainloop
                 
                 jmp     main                    ; Endlosschleife
                 nop                             ; sinnloser letzter Befehl, damit der IAR Simulator nicht meckert...
-
-;+++ PORT1_ISR ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++                                         
-PORT1_ISR
-                mov.w   #50000,R15  
-Entprellen      dec.w   R15                     ; Decrement R15
-                jnz     Entprellen              ; Springe zu Entprellen wenn R15 noch nicht 0 ist
-                
-                ; Senden eines Bytes
-                mov.b   R5,&UCA1TXBUF           ; hier ein ASCII Zeichen senden per UART bei jedem Tastendruck, indem das zu sendende Byte in den Sendepuffer kopiert wird
-                
-                ; auf nächstes ASCII-Zeichen weiterzählen für nächsten Tastendruck
-                inc R5                          ; R5 um 1 erhöhen = nächstes Zeichen aus der ASCII Tabelle in R5 laden, welches beim nächsten Tastendruck gesendet wir
-                cmp.w   #0x3a, R5               ; prüfen ob maximaler Zählerstand für R5 erreicht wurde (es sollen ja nur die Zeichen 0...9 gesendet werden)        
-                jnz Nicht_zu_Null_ruecksetzen   ; falls maximaler Zählerstand nicht erreicht wurde: R5 nicht zurücksetzen auf 0x30 = ASCII-"0"
-                mov.w   #0x30, R5               ; falls maximaler Zählerstand erreicht wurde: R5 auf 0x30 rücksetzen, dann wird beim nächsten Tastendruck wieder die ASCII-"0" gesendet        
-Nicht_zu_Null_ruecksetzen                
-                
-                
-                bic.b   #Button_S2, &P1IFG      ; das gesetzte Interruptflag löschen, sonst würde ISR sofort wieder neu ausgelöst werden
-               
-                reti    ; eine Interruptroutine muss immer mit dem Befehl reti abgeschlossen werden
-;+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 ;+++ UART_RxD_ISR ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 UART_RxD_ISR    nop
@@ -138,7 +107,7 @@ CALC            nop
 
 
 
-THREEDIGITS     ; 3 digits input
+THREEDIGITS                                 ; 3 digits input
                 add     R7, R10             ; add unit to R10
 
                 CLRZ
@@ -156,7 +125,7 @@ HUNDERTER       add     R5, R10             ; add hundred  to R10
                 JMP     OUTPUT              ; Calculation finished
 
 
-TWODIGITS       ; 2 digits input
+TWODIGITS                                   ; 2 digits input
                 add     R6, R10             ; add unit to R10
 
                 CLRZ
@@ -167,7 +136,7 @@ TEN2            add     R5, R10             ; add tens to R10
                 
                 JMP     OUTPUT              ; Calculation finished
                 
-ONEDIGIT        ; 1 digit input
+ONEDIGIT                                    ; 1 digit input
                 add     R5, R10             ; add unit to R10
 
                 JMP     OUTPUT
@@ -230,8 +199,6 @@ wait_for_byte3  bit.b   #UCTXIFG,&UCA1IFG       ; test if TX interrupt flag is s
             ORG     0FFFEh                  ; MSP430 RESET Vector
             DW      RESET                   
             
-            ORG     0FFDEh                  ;Interrupt Vektor für die Flags in Register P1IFG
-            DW      PORT1_ISR               ;die Interrupt Vektor Adresse 0xFFDE steht im Datenblatt des MSP430F5529 auf Seite 21 in der Tabelle
             
             ORG     0FFDCh                  ;Interrupt Vektor für die Flags UCA1RXIFG, UCA1TXIFG (USCI_A1 Receive or Transmit Interrupt)
             DW      UART_RxD_ISR            ;die Interrupt Vektor Adresse 0xFFDE steht im Datenblatt des MSP430F5529 auf Seite 21 in der Tabelle
